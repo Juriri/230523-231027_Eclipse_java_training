bbfe5616ba7cf591499eda10baaf98b752a42c2a
Juriri <lhj7826@naver.com>
2023-07-26 17:27:13 +0900

    20230726 17:27 업데이트

---
+++ 20230726_Multipart_assign/src/main/java/board/controller/ListController.java
@@ -0,0 +1,66 @@
+package board.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.service.Service;
+import board.service.ServiceImpl;
+import model.Board;
+
+/**
+ * Servlet implementation class ListController
+ */
+@WebServlet("/ListController")
+public class ListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ListController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("euc-kr");
+		response.setContentType("text/html; charset=euc-kr");
+		request.setCharacterEncoding("euc-kr");
+		
+		Service service = new ServiceImpl();
+		List<Board> list = service.getAll();
+		
+		request.setAttribute("list", list);
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/board/list.jsp");
+		if(dispatcher!=null) {
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
